03cb2de74a2a3b7a7997a3b10a23f97a01917525
Gaganpreet Arora <me@gaganpreet.in>
2020-11-01 18:38:34 +0100

    WIP: Playlist copy

--- main.py
+++ main.py
@@ -26,6 +26,13 @@ class Track:
     title: str
 
 
+@dataclass
+class Playlist:
+    title: str
+    id: str
+    count: Optional[int]
+
+
 class YoutubeMusic:
     def __init__(self):
         self.ytmusic = YTMusic("headers_auth.json")
@@ -36,19 +43,43 @@ class YoutubeMusic:
             for artist in self.ytmusic.get_library_subscriptions()
         ]
 
+    def get_playlists(self):
+        playlists = []
+        for playlist_data in self.ytmusic.get_library_playlists():
+            pprint(playlist_data)
+            playlists.append(
+                Playlist(
+                    title=playlist_data["title"],
+                    id=playlist_data["playlistId"],
+                    count=int(playlist_data.get("count", 0)),
+                )
+            )
+        return playlists
+
+    @staticmethod
+    def _track_data_to_track(track_data) -> Track:
+        artist = Artist(name=track_data["artists"][0]["name"])
+        album = None
+        if track_data["album"]:
+            album = Album(
+                title=track_data["album"]["name"],
+                artists=artist,
+                year=None,
+                type_="Album",
+            )
+        return Track(album=album, artist=artist, title=track_data["title"])
+
+    def get_playlist_tracks(self, playlist: Playlist):
+        return [
+            self._track_data_to_track(track_data)
+            for track_data in self.ytmusic.get_playlist(playlist.id)["tracks"]
+        ]
+
     def get_liked_songs(self):
         tracks = []
         for track_data in self.ytmusic.get_liked_songs()["tracks"]:
-            artist = Artist(name=track_data["artists"][0]["name"])
-            album = None
-            if track_data["album"]:
-                album = Album(
-                    title=track_data["album"]["name"],
-                    artists=artist,
-                    year=None,
-                    type_="Album",
-                )
-            tracks.append(Track(album=album, artist=artist, title=track_data["title"]))
+            track = self._track_data_to_track(track_data)
+            tracks.append(track)
         return tracks
 
     def get_albums(self):
@@ -99,6 +130,13 @@ class Spotify:
     def get_albums(self):
         return self.spotipy.current_user_saved_albums()
 
+    def _find_track(self, track: Track):
+        query = f'artist:"{track.artist.name}"  track:"{track.title}"'
+        if track.album:
+            query += f' album:"{track.album.title}"'
+        item = self._get_first_search_result(query, "track")
+        return item
+
     def add_album(self, album: Album):
         if album.type_ in ["Album", "EP"]:
             query = f'artist:"{album.artists.name}" album:"{album.title}"'
@@ -113,13 +151,24 @@ class Spotify:
             print(f"Ignoring unknown type {album}")
 
     def like_track(self, track: Track):
-        query = f'artist:"{track.artist.name}"  track:"{track.title}"'
-        if track.album:
-            query += f' album:"{track.album.title}"'
-        item = self._get_first_search_result(query, "track")
+        item = self._find_track(track)
         id = item["id"]
         if not self.spotipy.current_user_saved_tracks_contains(tracks=[id])[0]:
             self.spotipy.current_user_saved_tracks_add(tracks=[id])
+
+    def ensure_playlist_exists(self, playlist: Playlist) -> Playlist:
+        print(
+            self.spotipy.user_playlist_create(
+                user=self.spotipy.me()["id"], name=f"{playlist.title} - Import"
+            )
+        )
+
+    def add_tracks(self, playlist: Playlist, tracks: List[Track]):
+        ...
+
+    def create_playlist(self, playlist: Playlist, tracks: List[Track]):
+        created_playlist = self.ensure_playlist_exists(playlist)
+        self.add_tracks(created_playlist, tracks)
 
     def subscribe_to_artist(self, artist: Artist):
         query = f'artist:"{artist.name}"'
@@ -137,8 +186,10 @@ def main():
     #    spotify.add_album(album)
     # for artist in yt.get_subscribed_artists():
     #    spotify.subscribe_to_artist(artist)
-    for track in yt.get_liked_songs()[:1]:
-        pprint(spotify.like_track(track))
+    # for track in yt.get_liked_songs()[:1]:
+    #   pprint(spotify.like_track(track))
+    for playlist in yt.get_playlists()[:2]:
+        spotify.create_playlist(playlist, yt.get_playlist_tracks(playlist))
 
 
 if __name__ == "__main__":
