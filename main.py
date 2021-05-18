665a24b51073ba35d04da594f19e7ef74400cf10
Gaganpreet Arora <me@gaganpreet.in>
2021-05-19 01:00:14 +0200

    Run everything with limited slices

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ from portable.spotify import SpotifyLibrary
 def main():
     yt = YoutubeMusicLibrary()
     spotify = SpotifyLibrary()
-    # for album in yt.get_albums()[:5]:
-    #    spotify.add_album(album)
-    # for artist in yt.get_subscribed_artists():
-    #    spotify.subscribe_to_artist(artist)
-    # for track in yt.get_liked_songs()[:1]:
-    #   pprint(spotify.like_track(track))
-    for playlist in yt.get_playlists()[:2]:
-        pprint(spotify.get_playlists())
+    for album in yt.get_albums()[:2]:
+        spotify.add_album(album)
+    for artist in yt.get_subscribed_artists():
+        spotify.subscribe_to_artist(artist)
+    for track in yt.get_liked_songs()[:5]:
+        spotify.like_track(track)
+    for old_playlist in yt.get_playlists()[:2]:
+        new_playlist = spotify.create_playlist(old_playlist)
+        spotify.add_tracks_to_playlist(new_playlist, old_playlist.tracks)
 
 
 if __name__ == "__main__":
