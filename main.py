76ad41e003d26e8a79b115e2f505ccefa25bd973
Gaganpreet Arora <me@gaganpreet.in>
2021-05-19 21:34:27 +0200

    Remove all limits to migrate whole library

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ from portable.spotify import SpotifyLibrary
 def main():
     yt = YoutubeMusicLibrary()
     spotify = SpotifyLibrary()
-    for album in yt.get_albums()[:2]:
+    for album in yt.get_albums():
         spotify.add_album(album)
     for artist in yt.get_subscribed_artists():
         spotify.subscribe_to_artist(artist)
-    for track in yt.get_liked_songs()[:5]:
+    for track in yt.get_liked_songs():
         spotify.like_track(track)
-    for old_playlist in yt.get_playlists()[:2]:
+    for old_playlist in yt.get_playlists():
         new_playlist = spotify.create_playlist(old_playlist)
         spotify.add_tracks_to_playlist(new_playlist, old_playlist.tracks)
 
